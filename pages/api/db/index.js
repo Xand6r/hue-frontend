892f525a903ab9be7e16e48467eb8ea1b53df1a0
xander <shuaibualexander@gmail.com>
2022-08-08 12:42:42 +0100

    fix env bug

--- pages/api/db/index.js
+++ pages/api/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoDBconnectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.oocwe.mongodb.net/${process.env.MONGODB_DATABASE}`;
+const mongoDBconnectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.oocwe.mongodb.net/${process.env.MONGODB_DATABASE}`;
 export const connectToDB = () =>
   mongoose.connect(mongoDBconnectionString, {
     useNewUrlParser: true,
