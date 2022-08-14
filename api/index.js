81903fc8ee6d6e066494e68d1432767b46806681
xander <shuaibualexander@gmail.com>
2022-08-14 18:56:21 +0100

    add more changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const BASE_URL =
-  process.env.ENVIRONMENT === "local" ? "http://localhost:3000/api" : "https://hue-frontend.vercel.app//api";
+  process.env.ENVIRONMENT === "local" ? "http://localhost:3000/api" : "https://hue-frontend.vercel.app/api";
 
 export const getRequest = (path) => {
   return axios.get(`${BASE_URL}${path}`);
